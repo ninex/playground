e07c1034ee15d39dd258ac7fd0927e5a5e4d40f7
Abrie Greeff <abriegreeff@gmail.com>
2012-04-24 15:54:08 +0200

    added signalR test project using mvc3

---
+++ SignalRTest/Models/TicketHub.cs
@@ -0,0 +1,20 @@
+﻿using SignalR.Hubs;
+
+namespace SignalRTest.Models
+{
+    public class TicketHub : Hub
+    {
+        int TotalTickets = 11;
+
+        public void GetTicketCount()
+        {
+            Clients.updateTicketCount(TotalTickets);
+        }
+        public void BuyTicket()
+        {
+            if (TotalTickets > 0)
+                TotalTickets -= 1;
+            Clients.updateTicketCount(TotalTickets);
+        }
+    }
+}
