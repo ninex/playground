0fb554e89e631dedb541a8cb5213ed59afa44f5f
Abrie Greeff <abriegreeff@gmail.com>
2013-01-15 22:41:38 +0200

    View stocks

--- MomentumWeb/Models/StockQuarter.cs
+++ MomentumWeb/Models/StockQuarter.cs
@@ -8,6 +8,7 @@ namespace MomentumWeb.Models
     public class StockQuarter
     {
         public string Ticker { get; set; }
+        public string FullName { get; set; }
 
         public double Quarter1 { get; set; }
         public double Quarter1Move { get; set; }
@@ -22,5 +23,11 @@ namespace MomentumWeb.Models
         public double Quarter4Move { get; set; }
 
         public double YearMove { get; set; }
+
+        public bool Quarter1Top5 { get; set; }
+        public bool Quarter2Top5 { get; set; }
+        public bool Quarter3Top5 { get; set; }
+        public bool Quarter4Top5 { get; set; }
+        public bool YearTop5 { get; set; }
     }
 }
